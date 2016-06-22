964418cbe06443c09cc39efd2d079f8974f16568
iMac_24 <koawna@gmail.com>
2016-06-22 12:02:00 +0700

    ReFresh

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    //Explicit
+    var intNumber:Int = 1
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   //Mainfunction
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }   //Second Function
+    
+    @IBAction func decreaseButton(sender: AnyObject) {
+        
+        intNumber -= 1
+        shownumber(intNumber)
+        
+    }   //IncreaseButton
+    
+   
+    @IBAction func increaseButton(sender: AnyObject) {
+        
+        intNumber += 1
+        shownumber(intNumber)
+        
+    }   //DecreaseButton
+    
+    func shownumber(myNumber:Int) -> Void {
+        print("myNumber = \(myNumber)")
     }
+    
 
 
-}
+}   //Main Class
 
